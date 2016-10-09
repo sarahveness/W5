00866084abd89ffc5ff47450b8e8a1c72255890d
Sarah Veness <sarahhveness@gmail.com>
2016-10-09 02:22:58 +0000

    initial commit

---
+++ musiclibrary.js
@@ -0,0 +1,53 @@
+function Library(name, creator) {
+  this.name = name;
+  this.creator = creator;
+};
+
+function Playlist(name=[]) {
+  this.name = name;
+};
+
+function Track() {
+  this.title = title;
+  this.rating = rating;
+  this.length = length;
+};
+
+
+Playlist.prototype.overallRating = function() {
+
+}
+
+Playlist.prototype.totalDuration = function() {
+
+}
+
+// var playlist = {
+//   name: ''
+// }
+
+// Object.setPrototypeOf(Library, playlist);
+
+// console.log(playlist.creator);
+
+// Library.prototype.name = function()
+
+// var playlist = new Library("playlistname", );
+// this.name = name;
+// this.track = track;
+
+// console.log(playlist);
+
+// function Playlist() {
+//   this.name
+
+// }
+
+// function Track () {
+//   this.title
+//   this.rating
+//   this.length
+
+
+
+// var
